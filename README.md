5267dccbbd7db76de6603471efdb86c3c1ba802a
reds6188 <reds6188@gmail.com>
2026-06-02 20:11:54 +0200

    Updated numbers.dart

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Script per imparare il linguaggio di programmazione Dart
 
 ## numbers.py
-Utilizzo dei numeri
+Utilizzo dei numeri:
+- Numeri interi `int` e reali `dobule`
+- Parsing di un numero intero/reale da una stringa
--- numbers.dart
+++ numbers.dart
@@ -1,10 +1,20 @@
 void main() {
-  int a = 10;
-  double b = 2.5;
-  int hex = 0xABCD;
-  double flt = 1.23e4;
-  
-  //int c = a + b;        // Will raise an error (can't)
-  double c = a + b;
-  print('$a + $b = $c');  // Output: 10 + 2.5 = 12.5
+	int a = 10;
+	double b = 2.5;
+	int hex = 0xABCD;
+	double flt = 1.23e4;
+
+	//int c = a + b;        // Will raise an error
+	double c = a + b;
+	print('$a + $b = $c');  // Output: 10 + 2.5 = 12.5
+	print(hex);				// Output: 43981
+	print(flt);				// Output: 12300.0
+
+	// Parsing int from a string ------------------------------
+	int d = int.parse('12');
+	print(a + d);			// Output: 22
+
+	// Parsing double from a string ------------------------------
+	double e = double.parse('1.2');
+	print(b + e);			// Output 3.7
 }
